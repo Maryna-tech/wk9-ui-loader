8e4ca6ae0b7e2eedae23befb7c21263f8390aaae
Maryna-tech <mgaraeva41@gmail.com>
2024-11-18 19:43:48 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useEffect,useState } from 'react';
+import LoaderPage from './Loader/LoaderPage';
+
+function App() {
+  const[stateLoader, setStateLoader] = useState(true);
+
+    useEffect(()=>{
+      const timer = setTimeout(()=>setStateLoader(false),3000);
+      return()=>clearTimeout(timer)
+  },[] )
+
+  return (
+    <div>
+      {stateLoader &&<LoaderPage/>}
+    </div>
+  );
+}
+
+export default App;
